2b2ead6946d28a966c460ebeb5a75e162b5a9ca7
Stackbit Code Editor <support@stackbit.com>
2021-02-21 00:18:36 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     siteMetadata: {
         title: `Rui Jie Wang`,
-        name: `Narative`,
+        name: arative`N`,
         siteUrl: `https://gatsby-theme-novela.netlify.com`,
         description: `This is my description that will be used in the meta tags and important for search results`,
 
